9fb289fb7b6bbff8893e2e96026a4f9392be4b35
editeodoro <enrico.diteodoro@gmail.com>
2018-02-14 17:04:26 +1100

    delete stuff

--- compileall.sh
+++ compileall.sh
@@ -1,25 +1,42 @@
-module load intel
-module load intelmpi
-module load python
-module load boost
-module load gsl
-module load fftw
-module load szip
-module load zlib/1.2.8--gnu--6.1.0
-module load hdf5
-module load profile/astro
-module load cfitsio
+SYSTEM=${1}
+
+
+if [[ ${SYSTEM} == "marconi" ]]
+then
+    module load intel
+    module load intelmpi
+    module load python
+    module load boost
+    module load gsl
+    module load fftw
+    module load szip
+    module load zlib/1.2.8--gnu--6.1.0
+    module load hdf5
+    module load profile/astro
+    module load cfitsio
+
+    GRACKLE_DIR=${HOME}/GIZMO-CMZ/grackle
+    SLUG_DIR=${HOME}/GIZMO-CMZ/slug2
+    
+    echo Compiling grackle and slug for ${SYSTEM}
+elif  [[ ${SYSTEM} == "darwin" ]]
+    
+fi
+
+
+#GRACKLE_DIR=${PWD}/grackle
+#SLUG_DIR=${PWD}/GIZMO-CMZ/slug2
 
 # Compile grackle
-cd ${HOME}/GIZMO-CMZ/grackle 
-./configure 
-cd src/clib 
-make machine-marconi
-make clean 
-make -j 24 
-make install
+#cd ${GRACKLE_DIR}
+#./configure 
+#cd src/clib 
+#make machine-marconi
+#make clean 
+#make -j 24 
+#make install
 
 # Compile slug
-cd ${HOME}/GIZMO-CMZ/slug2
-make clean
-make lib -j 24 MPI=ENABLE_MPI GSLVERSION=2 MACHINE=marconi FITS=ENABLE_FITS
+#cd ${HOME}/GIZMO-CMZ/slug2
+#make clean
+#make lib -j 24 MPI=ENABLE_MPI GSLVERSION=2 MACHINE=marconi FITS=ENABLE_FITS
--- grackle/src/clib/auto_show_version.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void auto_show_version(FILE *fp) {
-   fprintf (fp,"\n");
-   fprintf (fp,"The Grackle Version 3.0\n");
-   fprintf (fp,"Mercurial Branch   default\n");
-   fprintf (fp,"Mercurial Revision 461945e5cd9c+\n");
-   fprintf (fp,"\n");
-}
